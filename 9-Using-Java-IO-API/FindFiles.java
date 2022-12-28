fe51883cd8b28c34d1972b4b75781e17cc635e69
Igor Balanici <igor.balanici@gmail.com>
2022-12-28 14:11:34 +0200

    traverse directory

---
+++ 9-Using-Java-IO-API/FindFiles.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FindFiles {
+
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("./");
+        long minSize = 1_000;
+        try (var s = Files.find(path, 10,
+            (p, a) -> a.isRegularFile()
+        && p.toString().endsWith(".java")
+        && a.size() > minSize))
+        {
+            s.forEach(System.out::println);
+        }
+    }
+
+}
